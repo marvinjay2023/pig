7742a75f626178923b030f0380952ecef9b801c9
Marvin Rubio <marvinjay.rubio@ssu.edu.ph>
2025-01-05 20:14:18 +0800

    remove comment in index.php

--- index.php
+++ index.php
@@ -5,6 +5,4 @@ const BASE_PATH = __DIR__;
 require 'setting/system.php';
 require 'Router.php';
 
-//NOTE
-/* ha every week na report na Count an pigs na na add.? Pagka alala ko diri pa adto an vaccine, quarantine Pala adto.  */
 
